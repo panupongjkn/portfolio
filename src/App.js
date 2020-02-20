c1f91d94be1b64859dd7c683c977384906f23867
panupongjkn <panupong.jkn@gmail.com>
2020-02-21 02:28:00 +0700

    [UPDATE] add component expereince

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Navbar from './Components/Navbar'
 import Profile from './Components/Profile'
 import Technical from './Components/Technical'
+import Expereince from './Components/Expereince'
 
 class App extends React.Component {
   render() {
@@ -24,7 +25,37 @@ class App extends React.Component {
         languages: ['HTML', 'CSS', 'Javascript', 'Go', 'SQL'],
         frameworkLibrarys: ['ReactJS', 'Bootstrap', 'Echo'],
         tools: ['SourceTree', 'Git', 'NodeJS', 'Visual Studio Code', 'Figma']
-      }
+      },
+      expereinces: [
+        {
+          name: 'Wip Camp#10',
+          link: ['https://github.com/wipcamp/10-frontend'],
+          detail: 'Create website for IT Camp of the School of Information Technology at KMUTT.',
+          position: 'Front-end Developer(CSS)',
+          developed: 'React(Next.js) , Styled-components',
+        },
+        {
+          name: 'KMUTT Passport',
+          link: ['https://github.com/alchemist-itbangmod/passport2018'],
+          detail: 'Create website for managing activity in KMUTT.',
+          position: 'Front-end Developer',
+          developed: 'React(Next.js) , Styled-components, Axios, Bootstrap',
+        },
+        {
+          name: 'Wip Camp#11',
+          link: ['https://github.com/wipcamp/11-line-chatbot'],
+          detail: 'Create LINE chat bot for register IT Camp of the School of Information Technology at KMUTT.',
+          position: 'Front-end Developer',
+          developed: 'ReactJS, Styled-components, Axios, Bootstrap, Dialogflow, LINE API',
+        },
+        {
+          name: 'Internship at Innovasive Co., Ltd. (July 2019)',
+          link: ['https://github.com/wipcamp/10-frontend', 'https://github.com/panupongjkn/be_booking'],
+          detail: 'Create website for booking sign in KMUTT.',
+          position: 'Front-end and Back-end Developer',
+          developed: 'ReactJS, Styled-components, Axios, Bootstrap, Golang(Echo)',
+        },
+      ]
     }
     return (
       <div className="App">
@@ -38,6 +69,9 @@ class App extends React.Component {
           <div className='container py-5'>
             <Technical technicals={profile.technicals} />
           </div>
+        </div>
+        <div className='container py-5'>
+          <Expereince expereinces={profile.expereinces} />
         </div>
       </div>
     );
