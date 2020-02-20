d5057c3bb9a703301287cf6a867a828c13e295e2
panupongjkn <panupong.jkn@gmail.com>
2020-02-21 01:13:54 +0700

    [UPDATE] color navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = (props) => {
     return (
-        <nav className="navbar navbar-light bg-light">
+        <nav className="navbar navbar-dark bg-dark">
             <a className="navbar-brand" href="/">
                 {props.title}
             </a>
