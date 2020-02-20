58f1a569fe63ea99caa1a1e3557f7e95667911fb
panupongjkn <panupong.jkn@gmail.com>
2020-02-21 01:58:18 +0700

    [ADD] Component Technical

---
+++ src/Components/Technical.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Card = (props) => {
+    return (
+        <div className="card p-1" style={{ height: '200px' }}>
+            <img className="card-img-top" style={{ objectFit: 'contain', height: '150px' }} src={`/img/${props.technical}.png`} alt="Card image cap" />
+            <div className="card-body">
+                <p className="card-text text-dark text-center">{props.technical}</p>
+            </div>
+        </div>
+    )
+}
+
+const TechnicalList = (props) => {
+    return (
+        <div className='row pt-5'>
+            <h5>{props.name}</h5>
+            <div className='row'>
+                {props.list.map((tectnical) => {
+                    return <div className='col-6 col-sm-3 p-2'><Card technical={tectnical} /></div>
+                })}
+            </div>
+        </div>
+    )
+}
+const Technical = (props) => {
+    let technicals = props.technicals
+    return (
+        <div>
+            <h4 className={'text-center'}>My Technical</h4>
+            <TechnicalList name={'Languages'} list={technicals.languages} />
+            <TechnicalList name={'Framework & Library'} list={technicals.frameworkLibrarys} />
+            <TechnicalList name={'Tools'} list={technicals.tools} />
+        </div>
+    )
+}
+
+export default Technical
