288f865abdb2566a5e2f15ae7b3aa286f51014cf
panupongjkn <panupong.jkn@gmail.com>
2020-02-21 01:58:42 +0700

    [UPDATE] add component technical

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Navbar from './Components/Navbar'
 import Profile from './Components/Profile'
+import Technical from './Components/Technical'
 
 class App extends React.Component {
   render() {
@@ -18,19 +19,24 @@ class App extends React.Component {
       },
       religion: 'Buddhist',
       nationality: 'Thai',
-      hobbies : 'Playing football, Playing game, Watching movie and serie',
-      technical : {
-        language : ['HTML', 'CSS', 'Javascript', 'Go', 'SQL'],
-        frameworkLibrary : ['ReactJS', 'Bootstrap', 'Echo'],
-        tools : ['SourceTree', 'Git', 'NodeJS', 'Visual Studio Code', 'Figma']
+      hobbies: 'Playing football, Playing game, Watching movie and serie',
+      technicals: {
+        languages: ['HTML', 'CSS', 'Javascript', 'Go', 'SQL'],
+        frameworkLibrarys: ['ReactJS', 'Bootstrap', 'Echo'],
+        tools: ['SourceTree', 'Git', 'NodeJS', 'Visual Studio Code', 'Figma']
       }
     }
     return (
       <div className="App">
         <Navbar title={'Portfolio'} />
         <div className='container'>
-          <div className='mt-5'>
+          <div className='py-5'>
             <Profile profile={profile} />
+          </div>
+        </div>
+        <div className='text-light bg-dark'>
+          <div className='container py-5'>
+            <Technical technicals={profile.technicals} />
           </div>
         </div>
       </div>
